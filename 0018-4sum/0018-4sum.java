bd0e6770f852366223de3e113673fba93ffa4248
D BAVAJAN <bavajanbavajan746@gmail.com>
2025-11-19 20:19:17 +0530

    Time: 21 ms (20.53%), Space: 45.5 MB (28.26%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,24 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-    HashSet<List<Integer>> set = new HashSet<>();
-    if (n < 4) return new ArrayList<>();
-       for (int i = 0; i < n-3; i++) {
-        for (int j = i+1; j < n-2; j++) {
-            for (int k =  j+1; k < n-1; k++) {
-                for (int l = k+1; k < n; k++) {
-                    int sum = nums[i]+ nums[j]+ nums[k]+ nums[l];
+        List<List<Integer>> res = new ArrayList<>();
+        if (n < 4) return res;
+
+        Arrays.sort(nums); // required for two-pointer and duplicate skipping
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate j
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
                     if (sum == target) {
-                        List<Integer> list1 = new ArrayList<>(Arrays.asList( nums[i], nums[j], nums[k], nums[l]));
-                         Collections.sort(list1);
-                         set.add(list1);
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++; right--;
+                        // skip duplicates for left & right
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
-                  
-                 
                 }
             }
         }
-       }
-      return new ArrayList<>(set);
+        return res;
     }
 }
